46acf9a15cb465c765d39269d529090f3bfadc02
dotuandat-real <tuandatyc090304@gmail.com>
2026-03-05 11:30:02 +0700

    Initialize fresh-market-shop project

---
+++ fresh-market-backend/src/test/java/com/dotuandat/thesis/freshmarket/FreshMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotuandat.thesis.freshmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
